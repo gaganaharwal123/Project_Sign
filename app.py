d8d58c6ff983a9a186a2457167ea5db58b3fd6dd
Gagan agarwal <49319243+gaganaharwal123@users.noreply.github.com>
2021-07-14 11:12:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def gen_frames():
     cv2.destroyAllWindows()
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('templates/index.html')
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
