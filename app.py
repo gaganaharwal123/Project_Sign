cf4bc79bb7cc959ef25283975512b7bb1386f1a4
Gagan agarwal <49319243+gaganaharwal123@users.noreply.github.com>
2021-07-14 13:29:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, render_template, Response
 import cv2
-
+import streamlit as st
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 from collections import deque
@@ -15,6 +15,9 @@ app = Flask(__name__)
 
 
 def gen_frames():  
+    '''
+    Function to detect faces/eyes and smiles in the image passed to this function
+    '''
     mlp_model = load_model('emnist_mlp_model.h5')
     # cnn_model = load_model('emnist_cnn_model.h5')
 
@@ -103,14 +106,53 @@ def gen_frames():
 
     camera.release()
     cv2.destroyAllWindows()
-@app.route('/')
-def index():
-    return render_template('templates/index.html')
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-if __name__ == '__main__':
-    app.run(debug=False)
+def about():
+	st.write(
+		'''
+		**Haar Cascade** is an object detection algorithm.
+		It can be used to detect objects in images or videos. 
+		The algorithm has four stages:
+			1. Haar Feature Selection 
+			2. Creating  Integral Images
+			3. Adaboost Training
+			4. Cascading Classifiers
+Read more :point_right: https://docs.opencv.org/2.4/modules/objdetect/doc/cascade_classification.html
+https://sites.google.com/site/5kk73gpu2012/assignment/viola-jones-face-detection#TOC-Image-Pyramid
+		''')
+def main():
+    st.title("Sign verification App :sunglasses: ")
+    st.write("**Using the Haar cascade Classifiers**")
+
+    activities = ["Home", "About"]
+    choice = st.sidebar.selectbox("Pick something fun", activities)
+
+    if choice == "Home":
+
+    	st.write("Go to the About section from the sidebar to learn more about it.")
+        gen_frames()
+        # You can specify more file types below if you want
+#     	image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
+
+#     	if image_file is not None:
+
+#     		image = Image.open(image_file)
+
+#     		if st.button("Process"):
+                
+#                 # result_img is the image with rectangle drawn on it (in case there are faces detected)
+#                 # result_faces is the array with co-ordinates of bounding box(es)
+#     			result_img, result_faces = detect(image=image)
+#     			st.image(result_img, use_column_width = True)
+#     			st.success("Found {} faces\n".format(len(result_faces)))
+
+    elif choice == "About":
+    	about()
+
+
+
+
+if __name__ == "__main__":
+    main()
 # def sign_create():
 
 
